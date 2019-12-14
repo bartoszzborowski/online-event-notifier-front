56c81849caeacae446959ba902d08ac5461e0550
Althain <hajduczek1007@gmail.com>
2019-12-14 19:36:51 +0100

    Added notificationArea in the homePage

---
+++ src/components/NotificationArea/NotificationArea.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./NotificationArea.scss";
+import Toast from "react-bootstrap/Toast";
+import {Link} from "react-router-dom";
+
+export class NotificationArea extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filtersOpened: false
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (<>
+      <div aria-live="polite" aria-atomic="true" className={"notification-area offset-sm-9 col-sm-3"}>
+          <Toast
+              // onClose={() => this.toggleToast()}
+              // show={showToast}
+              // delay={3000}
+              // autohide
+          >
+            <Toast.Header>
+              <strong className="mr-auto">Event name</strong>
+              <small>On 11.11.2019</small>
+            </Toast.Header>
+            <Toast.Body>
+              Event's description... <Link to="./listView/1">Check now</Link>
+            </Toast.Body>
+          </Toast>
+
+        <Toast
+            // onClose={() => this.toggleToast()}
+            // show={showToast}
+            // delay={3000}
+            // autohide
+        >
+          <Toast.Header>
+            <strong className="mr-auto">Event name</strong>
+            <small>On 11.11.2019</small>
+          </Toast.Header>
+          <Toast.Body>
+            Event's description... <Link to="./listView/1">Check now</Link>
+          </Toast.Body>
+        </Toast>
+      </div>
+    </>);
+  }
+}
--- src/routes/HomePage/HomePage.jsx
+++ src/routes/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MapContainer from "../../components/MapContainer/MapContainer";
 import { TopNavigation } from "../../components/TopNavigation";
+import { NotificationArea } from "../../components/NotificationArea";
 
 class HomePage extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class HomePage extends React.Component {
       <div className="vh-100">
         <TopNavigation />
         <MapContainer />
+        <NotificationArea/>
       </div>
     );
   }
