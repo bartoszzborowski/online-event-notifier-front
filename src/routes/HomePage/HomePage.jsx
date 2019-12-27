329d89249e9d3fae76b19549fd14610c5f957c32
Althain <hajduczek1007@gmail.com>
2019-12-27 20:24:32 +0100

    Fixed loading in HomePage.jsx

--- src/routes/HomePage/HomePage.jsx
+++ src/routes/HomePage/HomePage.jsx
@@ -1,50 +1,41 @@
 import React from "react";
 import MapContainer from "../../components/MapContainer/MapContainer";
-import { TopNavigation } from "../../components/TopNavigation";
-import { NotificationArea } from "../../components/NotificationArea";
-import { eventActions } from "../../stores/actions";
-import { connect } from "react-redux";
+import {TopNavigation} from "../../components/TopNavigation";
+import {eventActions} from "../../stores/actions";
+import {connect} from "react-redux";
 import Loader from "react-loader-spinner";
 
 class HomePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // @TODO probably to be replaced by something else
-      loading: true
-    }
-  }
-
   componentDidMount() {
-    const { getByUser } = this.props;
+    const {getByUser} = this.props;
     if (getByUser) {
       getByUser();
     }
   }
 
   render() {
-    const { events } = this.props;
+    const {events, loading} = this.props;
 
     return (
-      <div className="vh-100">
-        <TopNavigation />
-        { this.state.loading && <div className={"d-flex align-items-center h-100 justify-content-center"}>
-          <Loader
-              type="Triangle"
-              color="#00BFFF"
-              height={100}
-              width={100}
-          />
-        </div>}
-        {!this.state.loading && <MapContainer events={events} />}
-      </div>
+        <div className="vh-100">
+          <TopNavigation/>
+          {loading && <div className={"d-flex align-items-center h-100 justify-content-center"}>
+            <Loader
+                type="Triangle"
+                color="#00BFFF"
+                height={100}
+                width={100}
+            />
+          </div>}
+          {!loading && events && <MapContainer events={events}/>}
+        </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  const { events, loading, searchedEvents } = state.events;
-  return { events, loading, searchedEvents };
+  const {events, loading, searchedEvents} = state.events;
+  return {events, loading, searchedEvents};
 };
 
 const actionCreators = {
@@ -52,7 +43,7 @@ const actionCreators = {
 };
 
 const connectedRegisterPage = connect(
-  mapStateToProps,
-  actionCreators
+    mapStateToProps,
+    actionCreators
 )(HomePage);
-export { connectedRegisterPage as HomePage };
+export {connectedRegisterPage as HomePage};
