9e4d531a1b3512c131c53ee9a9eceac8cc809462
Althain <hajduczek1007@gmail.com>
2019-12-27 22:25:57 +0100

    Improved notificationArea

--- src/components/NotificationArea/ToastItem.jsx
+++ src/components/NotificationArea/ToastItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./NotificationArea.scss";
 import Toast from "react-bootstrap/Toast";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 export class ToastItem extends React.Component {
   constructor(props) {
@@ -13,27 +13,29 @@ export class ToastItem extends React.Component {
 
   toggleToast() {
     this.setState(state => {
-      return { showToast: !state.showToast };
+      return {showToast: !state.showToast};
     });
   }
 
   render() {
-    const { showToast } = this.state;
-    const { event } = this.props;
-    const { id, event_date, name, description } = event;
+    const {showToast} = this.state;
+    const {event} = this.props;
+    const {id, event_date, name, description} = event;
     const eventLink = `./listView/${id}`;
     return (
-      <div>
-        <Toast onClose={() => this.toggleToast()} show={showToast} className={"mb-2"}>
-          <Toast.Header>
-            <strong className="mr-auto">{name}</strong>
-            <small>{event_date}</small>
-          </Toast.Header>
-          <Toast.Body>
-            {description} <Link to={eventLink}>Check now</Link>
-          </Toast.Body>
-        </Toast>
-      </div>
+        <>
+          {showToast && <div>
+            <Toast onClose={() => this.toggleToast()} show={showToast} className={"mb-2"}>
+              <Toast.Header>
+                <strong className="mr-auto">{name}</strong>
+                <small>{event_date}</small>
+              </Toast.Header>
+              <Toast.Body>
+                {description} <Link to={eventLink}>Check now</Link>
+              </Toast.Body>
+            </Toast>
+          </div>}
+        </>
     );
   }
 }
